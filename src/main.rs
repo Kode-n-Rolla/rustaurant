0df94fe04b7838afb1db5114bfaca048545bde58
shinobi <shinobi@localhost.localdomain>
2026-03-25 18:39:42 -0300

    feat: init rustaurant and find tables function

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use std::io;
-use restaurant::model;
+
+mod restaurant;
+use crate::restaurant::logic::{user_input_guest, create_rustaurant, find_table};
+use crate::restaurant::model::{Restaurant, RestaurantStatus};
 
 fn main() {
     println!("Welcome to the Rustaurant");
@@ -8,15 +11,48 @@ fn main() {
 
     let mut choice: String = String::new();
 
-    io::stdin().read_line(&mut choice).expect("Failed to understand");
+    io::stdin()
+        .read_line(&mut choice)
+        .expect("Failed to understand");
 
     let choice: u8 = match choice.trim().parse() {
         Ok(ch) => ch,
-        Err(_) => 255, //@todo imlp correct out value
+        Err(_) => {
+            println!("Invalid input");
+            return;
+        }
     };
 
-    match choice {
-        1 => println!("Rustaurant is open"),
-        _ => println!("Rustaurant is closed")
+    if choice == 1 {
+        let work_until: u32 = 5; //@todo user input
+        let mut restaurant = create_rustaurant();
+        rustaurant_start(&mut restaurant, work_until);
+    } else {
+        println!("Rustaurant is closed");
+    }
+}
+
+fn rustaurant_start(restaurant: &mut Restaurant, working: u32) {
+    println!("Rustaurant is open");
+
+    //println!("Tables {:?}", restaurant.tables);
+
+    // Main work loop
+    loop {
+        println!("Working {} ticks", restaurant.tick);
+
+        if restaurant.tick >= working {
+            restaurant.status = RestaurantStatus::Closed;
+            println!("Rustaurant is closed");
+            break;
+        }
+
+        println!("Somebody come.");
+        let count: u8 = user_input_guest();
+
+        // find table
+        find_table(restaurant, count);
+
+        restaurant.tick += 1;
     }
 }
---
+++ src/restaurant/logic.rs
@@ -0,0 +1,102 @@
+use std::io;
+
+use crate::restaurant::model::{Restaurant, RestaurantStatus, Table};
+
+pub fn user_input_guest() -> u8 {
+
+    println!("Input count of guest(s):");
+
+    let mut count = String::new();
+
+    io::stdin()
+        .read_line(&mut count)
+        .expect("Failed input");
+
+    let count: u8 = match count.trim().parse() {
+        Ok(c) => c,
+        Err(_) => {
+            return 0; // @todo imlp error handling
+        }
+    };
+
+    count
+}
+
+pub fn create_rustaurant() -> Restaurant {
+    let tables = create_tables();
+    let rustaurant = Restaurant {
+        tick: 0,
+        status: RestaurantStatus::Open,
+        tables,
+        waiting_queue: vec![],
+        occupy_tables: vec![]
+    };
+
+    rustaurant
+}
+
+pub fn find_table(restaurant: &mut Restaurant, count_of_guests: u8) { 
+    for i in restaurant.tables.iter_mut() {
+        if i.capacity >= count_of_guests {
+            i.remaining_ticks = 2;
+            restaurant.occupy_tables.push(i.id);
+        }
+    }
+}
+
+fn create_tables() -> Vec<Table> {
+    // 4 tables for 1 person
+    // let tables_for_1 = (1..=4).map(|id| Table {
+    //     id,
+    //     capacity: 1,
+    //     remaining_ticks: 0,
+    // });
+    // 4 tables for 2 person
+    // 4 tables for 3 person
+    // 3 tables for 4 person
+    // 2 tables for 5 person
+    // Total 17 tables
+    let mut tables: Vec<Table> = Vec::new();
+
+    // Add tabels for 1
+    for id in 1..=17 {
+        if id < 5 {
+            let table = Table {
+                id,
+                capacity: 1,
+                remaining_ticks: 0,
+            };
+            tables.push(table);
+        } else if id < 9 {
+            let table = Table {
+                id,
+                capacity: 2,
+                remaining_ticks: 0,
+            };
+            tables.push(table);
+        } else if id < 13 {
+            let table = Table {
+                id,
+                capacity: 3,
+                remaining_ticks: 0,
+            };
+            tables.push(table);
+        } else if id < 16 {
+            let table = Table {
+                id,
+                capacity: 4,
+                remaining_ticks: 0,
+            };
+            tables.push(table);
+        } else {
+            let table = Table {
+                id,
+                capacity: 5,
+                remaining_ticks: 0,
+            };
+            tables.push(table);
+        }
+    }
+
+    tables
+}
